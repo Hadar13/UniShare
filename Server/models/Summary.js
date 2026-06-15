a15fff99fb524252c0739dd9a1d3223e794ed1eb
Hadar13 <Yakutihadar13@gmail.com>
2026-06-16 00:31:30 +0300

    Update Summary.js

--- Server/models/Summary.js
+++ Server/models/Summary.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose');
 
+const summarySchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  university: {
+    type: String,
+    required: true
+  },
+
+  subject: {
+    type: String,
+    required: true
+  },
+
+  description: {
+    type: String,
+    maxlength: 500
+  },
+
+  fileUrl: {
+    type: String,
+    required: true
+  },
+
+  uploader: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Summary', summarySchema);
