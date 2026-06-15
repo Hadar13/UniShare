91b5900cd97ffbc7ea71988533b4df634d7eca0e
Hadar13 <Yakutihadar13@gmail.com>
2026-06-16 00:45:50 +0300

    Update summaryRoutes.js

--- Server/routes/summaryRoutes.js
+++ Server/routes/summaryRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const summaryController = require('../controllers/summaryController');
 
 router.get('/', summaryController.getAllSummaries);
+router.post('/', summaryController.createSummary);
 
 module.exports = router;
