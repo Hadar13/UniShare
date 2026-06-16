9c00e0207f9ba843f0276ab209aeb93b870114a3
Hadar13 <Yakutihadar13@gmail.com>
2026-06-16 19:53:54 +0300

    Complete summaries CRUD

--- Server/routes/summaryRoutes.js
+++ Server/routes/summaryRoutes.js
@@ -5,5 +5,9 @@ const summaryController = require('../controllers/summaryController');
 
 router.get('/', summaryController.getAllSummaries);
 router.post('/', summaryController.createSummary);
+router.get('/:id', summaryController.getSummaryById);
+router.put('/:id', summaryController.updateSummary);
+router.delete('/:id', summaryController.deleteSummary);
+
 
 module.exports = router;
