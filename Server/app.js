de5891c92632e7b210e43be1eb92b1a79eec69fa
Hadar13 <Yakutihadar13@gmail.com>
2026-06-18 15:49:21 +0300

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -4,11 +4,15 @@ const express = require('express');
 const cors = require('cors'); // הכנה לחיבור עתידי מול React
 const summaryRoutes = require('./routes/summaryRoutes'); // מביא את ה-routes של הסיכומים
 const authRoutes = require('./routes/authRoutes');
+const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 app.get('/', (req, res) => {
   res.send('UniShare server is running');
@@ -17,5 +21,7 @@ app.get('/', (req, res) => {
 // חיבור ה-routes של הסיכומים לשרת
 app.use('/api/summaries', summaryRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use(errorHandler);
 
 module.exports = app; // מייצאת את האפליקציה כדי ש־server.js יוכל להפעיל אותה
